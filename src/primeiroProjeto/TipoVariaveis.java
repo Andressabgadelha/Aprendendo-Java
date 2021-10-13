2819da9008912681a6311d8d72790949f4c0ee49
Andressabgadelha <de_gadelha@live.com>
2021-10-13 16:45:29 -0300

    commit inicial

---
+++ src/primeiroProjeto/TipoVariaveis.java
@@ -0,0 +1,24 @@
+package primeiroProjeto;
+
+public class TipoVariaveis {
+
+	public static void main(String[] args) {
+		byte tipoByte= 123;
+		short tipoShort= 12345;
+		char tipoChar= 'A';
+		float tipoFloat= 2.1f;
+		double tipoDouble= 1.52;
+		int tipoInt= 1234567;
+		long tipoLong= 1234567895l;
+		boolean tipoBooleano= true;
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoFloat = " + tipoFloat);
+		System.out.println("Valor do tipoDouble = " + tipoDouble);
+		System.out.println("Valor do tipoInt = " + tipoInt);
+		System.out.println("Valor do tipoLong = " + tipoLong);
+		System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+
+	}
+
+}
